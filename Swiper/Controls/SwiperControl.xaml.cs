fa327d02604e91af3b86136caf40fe726848b1f3
Patrick Petropoulos <patrickpetropoulos@gmail.com>
2024-03-21 01:51:04 -0400

    small changes

--- Swiper/Controls/SwiperControl.xaml.cs
+++ Swiper/Controls/SwiperControl.xaml.cs
@@ -6,6 +6,13 @@ public partial class SwiperControl : ContentView
 {
 	private readonly double _initialRotation;
 	private static readonly Random _random = new Random();
+	private double _screenWidth = -1;
+	private const double DeadZone = 0.4d;
+	private const double DecisionThreshold = 0.4d;
+
+	public event EventHandler OnLike;
+	public event EventHandler OnDeny;
+
 	public SwiperControl()
 	{
 		InitializeComponent();
@@ -25,6 +32,62 @@ public partial class SwiperControl : ContentView
 		loadingLabel.BindingContext = image;
 	}
 
+	protected override void OnSizeAllocated( double width, double height )
+  {
+    base.OnSizeAllocated( width, height );
+		if( Application.Current.MainPage == null )
+		{
+			return;
+		}
+		_screenWidth = Application.Current.MainPage.Width;
+  }
+
+	public void CalculatePanState( double panX )
+	{
+		var halfScreenWidth = _screenWidth / 2;
+		var deadZoneEnd = DeadZone * halfScreenWidth;
+		if(Math.Abs(panX) < deadZoneEnd)
+    {
+      return;
+    }
+		var passedDeadZone = panX < 0 ? panX + deadZoneEnd : panX - deadZoneEnd;
+
+		var decisionZoneEnd = DecisionThreshold * halfScreenWidth;
+		var opacity = passedDeadZone / decisionZoneEnd;
+		opacity = double.Clamp( opacity, -1, 1 );
+
+		likeStackLayout.Opacity = opacity;
+		denyStackLayout.Opacity = -opacity;
+	}
+
+	private bool CheckForExitCriteria()
+	{
+		var halfScreenWidth = _screenWidth / 2;	
+		var decisionBreakpoint = DeadZone * halfScreenWidth;
+		return (Math.Abs(photo.TranslationX) > decisionBreakpoint);
+	}
+
+	private void Exit()
+	{
+		MainThread.BeginInvokeOnMainThread( async () =>
+		{
+			var direction = photo.TranslationX < 0? -1 : 1;
+
+			if( direction > 0 )
+			{
+				OnLike?.Invoke( this, new EventArgs() );
+			}
+			if( direction < 0 )
+			{
+				OnDeny?.Invoke( this, new EventArgs() );
+			}
+
+			await photo.TranslateTo( photo.TranslationX + ( _screenWidth * direction ), photo.TranslationY, 200, Easing.CubicIn );
+			var parent = Parent as Layout;
+			parent?.Children.Remove( this );
+
+		} );
+	}
 	private void OnPanUpdated( object sender, PanUpdatedEventArgs e )
 	{
 		switch( e.StatusType )
@@ -50,9 +113,19 @@ public partial class SwiperControl : ContentView
 		photo.TranslationX = e.TotalX;
 		photo.TranslationY = e.TotalY;	
 		photo.Rotation = _initialRotation + (photo.TranslationX / 100);
+
+		CalculatePanState( e.TotalX );
 	}
 	private void PanCompleted()
   {
+		if( CheckForExitCriteria() )
+		{
+			Exit();
+		}
+
+		likeStackLayout.Opacity=0;
+		denyStackLayout.Opacity=0;
+
     photo.TranslateTo( 0, 0, 250, Easing.SpringOut );
     photo.RotateTo( _initialRotation, 250, Easing.SpringOut );
     photo.ScaleTo( 1, 250, Easing.SpringOut );
--- Swiper/Utils/Picture.cs
+++ Swiper/Utils/Picture.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Swiper.Utils;
 
-namespace Swiper.Utils;
-public class Picture
+internal class Picture
 {
   public Uri Uri { get; init; }
   public string Description { get; init; }
 
   public Picture()
   {
-    Uri = new Uri( "$https://picsum.photos/400/400/?random&ts={DateTime.Now.Ticks}" );
-    var generaor = new DescriptionGenerator();
-    Description = generaor.Generate();
+    Uri = new Uri( $"https://picsum.photos/400/400/?random&ts={DateTime.Now.Ticks}" );
+    var generator = new DescriptionGenerator();
+    Description = generator.Generate();
   }
 }
